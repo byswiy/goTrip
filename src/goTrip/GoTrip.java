64b5593db5a8fb99a00f6bf6a129175abac21077
byswiy <byswiy99@naver.com>
2022-04-12 20:25:41 +0900

    GoTrip.java : case 안에 쓸데없는 if문 삭제 및 들여쓰기 수정

--- src/goTrip/GoTrip.java
+++ src/goTrip/GoTrip.java
@@ -90,232 +90,222 @@ public class GoTrip {
 		case 1:
 			// 1번을 누르면 여행 경비를 입력할 수 있는 기능 실행
 			// 해당하는 금액에 따라 여행지를 추천해준다
-			if(num == 1) {
-				
 			GoTrip.printline();
 			
 			// 반복문을 사용해서 금액이 맞지 않을 때 다시 재입력 할 수 있게 한다
 			GoTrip.travelExpenses();
-		}
 			break;
 		// 여행 경비 입력 후 여행지 추천 종료
 		
 		// 지역별 인기 추천 장소 실행 
 		case 2:
-			if(num == 2) {
-				ArrayList<String> firstRegion = new ArrayList<>();
-				firstRegion.add("경기도 -> 에버랜드, 한국민속촌, 쁘띠프랑스");
-				firstRegion.add("인천 -> 을왕리 해수욕장, 송도 센트럴파크, 인천 대공원");
-				firstRegion.add("서울 -> 복촌 한옥마을, 올림픽 공원, 낙산공원");
+			ArrayList<String> firstRegion = new ArrayList<>();
+			firstRegion.add("경기도 -> 에버랜드, 한국민속촌, 쁘띠프랑스");
+			firstRegion.add("인천 -> 을왕리 해수욕장, 송도 센트럴파크, 인천 대공원");
+			firstRegion.add("서울 -> 복촌 한옥마을, 올림픽 공원, 낙산공원");
 					
-				ArrayList<String> secondRegion = new ArrayList<>();
-				secondRegion.add("충청도 -> 꽂지 해수욕장, 남당항, 아산지중해");
-				secondRegion.add("세종 -> 세종 호수공원, 국립세종수목원, 베어트리파크");
-				secondRegion.add("대전 -> 국립중앙과학관, 대전아쿠아리움, 대전오월드");
+			ArrayList<String> secondRegion = new ArrayList<>();
+			secondRegion.add("충청도 -> 꽂지 해수욕장, 남당항, 아산지중해");
+			secondRegion.add("세종 -> 세종 호수공원, 국립세종수목원, 베어트리파크");
+			secondRegion.add("대전 -> 국립중앙과학관, 대전아쿠아리움, 대전오월드");
 				
-				ArrayList<String> thirdRegion = new ArrayList<>();
-				thirdRegion.add("강원도 -> 대관령 양떼 목장, 남이섬, 하슬라 아트월드");
+			ArrayList<String> thirdRegion = new ArrayList<>();
+			thirdRegion.add("강원도 -> 대관령 양떼 목장, 남이섬, 하슬라 아트월드");
 					
-				ArrayList<String> fourthRegion = new ArrayList<>();
-				fourthRegion.add("경상도 -> 동피랑 벽화마을, 바람의 언덕, 매미성");
-				fourthRegion.add("대구 -> 이월드, 83타워, 국립대구과학관");
-				fourthRegion.add("울산 -> 대왕암 공원, 간절곶, 신화마을");
-				fourthRegion.add("부산 -> 감천문화마을, 광안리해수욕장, 국립광주박물관");
+			ArrayList<String> fourthRegion = new ArrayList<>();
+			fourthRegion.add("경상도 -> 동피랑 벽화마을, 바람의 언덕, 매미성");
+			fourthRegion.add("대구 -> 이월드, 83타워, 국립대구과학관");
+			fourthRegion.add("울산 -> 대왕암 공원, 간절곶, 신화마을");
+			fourthRegion.add("부산 -> 감천문화마을, 광안리해수욕장, 국립광주박물관");
 					
-				ArrayList<String> fifthRegion = new ArrayList<>();
-				fifthRegion.add("전라도 -> 순천만 국가공원, 낙안읍성민속마을, 대흥사");
-				fifthRegion.add("광주 -> 펭귄마을, 국립광주과학관, 국립광주박물관");
+			ArrayList<String> fifthRegion = new ArrayList<>();
+			fifthRegion.add("전라도 -> 순천만 국가공원, 낙안읍성민속마을, 대흥사");
+			fifthRegion.add("광주 -> 펭귄마을, 국립광주과학관, 국립광주박물관");
 				
-				GoTrip.printline();
+			GoTrip.printline();
 				
-				System.out.println("1. 경기도 | 인천 | 서울");
-				System.out.println("2. 충청도 | 세종 | 대전");
-				System.out.println("3. 강원도");
-				System.out.println("4. 전라도 | 광주");
-				System.out.println("5. 경상도 | 대구 | 울산 | 부산");
+			System.out.println("1. 경기도 | 인천 | 서울");
+			System.out.println("2. 충청도 | 세종 | 대전");
+			System.out.println("3. 강원도");
+			System.out.println("4. 전라도 | 광주");
+			System.out.println("5. 경상도 | 대구 | 울산 | 부산");
 				
-				GoTrip.printline();
+			GoTrip.printline();
 					
-				System.out.print("알고싶은 지역의 번호 선택해주세요 -> ");
-				int number = scanf.nextInt();
+			System.out.print("알고싶은 지역의 번호 선택해주세요 -> ");
+			int number = scanf.nextInt();
 				
-				GoTrip.printline();
+			GoTrip.printline();
 				
-				if(number == 1) {
-					System.out.println("<경기도 | 인천 | 서울>");
-				} else if(number == 2) {
-					System.out.println("<충청도 | 세종 | 대전>");
-				} else if(number == 3) {
-					System.out.println("<강원도>");
-				} else if(number == 4) {
-					System.out.println("<전라도 | 광주>");
-				} else if(number == 5) {
-					System.out.println("<경상도 | 대구 | 울산 | 부산>");
-				} else {
-					System.out.println("번호를 잘못입력하셨습니다");
+			if(number == 1) {
+				System.out.println("<경기도 | 인천 | 서울>");
+			} else if(number == 2) {
+				System.out.println("<충청도 | 세종 | 대전>");
+			} else if(number == 3) {
+				System.out.println("<강원도>");
+			} else if(number == 4) {
+				System.out.println("<전라도 | 광주>");
+			} else if(number == 5) {
+				System.out.println("<경상도 | 대구 | 울산 | 부산>");
+			} else {
+				System.out.println("번호를 잘못입력하셨습니다");
+			}
+					
+					
+			if(number == 1) {
+				for(int i=0; i<firstRegion.size(); i++) {
+					String fR = firstRegion.get(i);
+					System.out.println((i + 1) + ". " + fR);
 				}
-					
-					
-				if(number == 1) {
-					for(int i=0; i<firstRegion.size(); i++) {
-						String fR = firstRegion.get(i);
-						System.out.println((i + 1) + ". " + fR);
-					}
-				} else if(number == 2) {
-					for(int i=0; i<secondRegion.size(); i++) {
-						String sR = secondRegion.get(i);
-						System.out.println((i + 1) + ". " + sR);
-					}
-				} else if(number == 3) {
-					for(int i=0; i<thirdRegion.size(); i++) {
-						String tR = thirdRegion.get(i);
-						System.out.println((i + 1) + ". " + tR);
-					} 
-				} else if(number == 4) {
-					for(int i=0; i<fourthRegion.size(); i++) {
-						String foR = fourthRegion.get(i);
-						System.out.println((i + 1) + ". " + foR);
-					} 
-				} else if(number == 5) {
-					for(int i=0; i<fifthRegion.size(); i++) {
-						String fiR = fifthRegion.get(i);
-						System.out.println((i + 1) + ". " + fiR);
-					} 
+			} else if(number == 2) {
+				for(int i=0; i<secondRegion.size(); i++) {
+					String sR = secondRegion.get(i);
+					System.out.println((i + 1) + ". " + sR);
 				}
+			} else if(number == 3) {
+				for(int i=0; i<thirdRegion.size(); i++) {
+					String tR = thirdRegion.get(i);
+					System.out.println((i + 1) + ". " + tR);
+				} 
+			} else if(number == 4) {
+				for(int i=0; i<fourthRegion.size(); i++) {
+					String foR = fourthRegion.get(i);
+					System.out.println((i + 1) + ". " + foR);
+				} 
+			} else if(number == 5) {
+				for(int i=0; i<fifthRegion.size(); i++) {
+					String fiR = fifthRegion.get(i);
+					System.out.println((i + 1) + ". " + fiR);
+				} 
 			}		
 			break;
 		// 인기있는 10군데 지역 추천 실행	
 		case 3:
-			if(num == 3) {
-				Region region1 = new Region("부산");
-				Region region2 = new Region("남해");
-				Region region3 = new Region("삼척");
-				Region region4 = new Region("서울");
-				Region region5 = new Region("전주");
-				Region region6 = new Region("공주");
-				Region region7 = new Region("순천");
-				Region region8 = new Region("인천");
-				Region region9 = new Region("제주도");
-				Region region10 = new Region("안동");
+			Region region1 = new Region("부산");
+			Region region2 = new Region("남해");
+			Region region3 = new Region("삼척");
+			Region region4 = new Region("서울");
+			Region region5 = new Region("전주");
+			Region region6 = new Region("공주");
+			Region region7 = new Region("순천");
+			Region region8 = new Region("인천");
+			Region region9 = new Region("제주도");
+			Region region10 = new Region("안동");
 				
 				
-				ArrayList<Region> recommendRegion= new ArrayList<>();
-				recommendRegion.add(region1);
-				recommendRegion.add(region2);
-				recommendRegion.add(region3);
-				recommendRegion.add(region4);
-				recommendRegion.add(region5);
-				recommendRegion.add(region6);
-				recommendRegion.add(region7);
-				recommendRegion.add(region8);
-				recommendRegion.add(region9);
-				recommendRegion.add(region10);
+			ArrayList<Region> recommendRegion= new ArrayList<>();
+			recommendRegion.add(region1);
+			recommendRegion.add(region2);
+			recommendRegion.add(region3);
+			recommendRegion.add(region4);
+			recommendRegion.add(region5);
+			recommendRegion.add(region6);
+			recommendRegion.add(region7);
+			recommendRegion.add(region8);
+			recommendRegion.add(region9);
+			recommendRegion.add(region10);
 				
-				GoTrip.printline();
-				System.out.println("<< 추천 top 10 >>");
+			GoTrip.printline();
+			System.out.println("<< 추천 top 10 >>");
 				
-				for(int i=0; i<recommendRegion.size(); i++) {
-					System.out.println("•" + recommendRegion.get(i));
-				}
-				GoTrip.printline();
-		}
+			for(int i=0; i<recommendRegion.size(); i++) {
+				System.out.println("•" + recommendRegion.get(i));
+			}
+				
+			GoTrip.printline();
 			break;
 		// 인기있는 10군데 지역 추천 종료
 			
 		// 검색한 지역 별 인기 시, 군 추천 기능창 실행
 		case 4:
-			if(num == 4) {
+			System.out.print("12개 지역 중 하나를 입력해주세요! -> ");
+			String wantRegion = scanf.next();
 				
-				System.out.print("12개 지역 중 하나를 입력해주세요! -> ");
-				String wantRegion = scanf.next();
+			GoTrip.printline();
 				
-				GoTrip.printline();
-				
-				SearchRegion wantR1 = new SearchRegion("강원도");
-				SearchRegion wantR2 = new SearchRegion("경기도");
-				SearchRegion wantR3 = new SearchRegion("인천");
-				SearchRegion wantR4 = new SearchRegion("서울");
-				SearchRegion wantR5 = new SearchRegion("충청도");
-				SearchRegion wantR6 = new SearchRegion("대전");
-				SearchRegion wantR7 = new SearchRegion("경상도");
-				SearchRegion wantR8 = new SearchRegion("대구");
-				SearchRegion wantR9 = new SearchRegion("울산");
-				SearchRegion wantR10 = new SearchRegion("부산");
-				SearchRegion wantR11 = new SearchRegion("전라도");
-				SearchRegion wantR12 = new SearchRegion("광주");
+			SearchRegion wantR1 = new SearchRegion("강원도");
+			SearchRegion wantR2 = new SearchRegion("경기도");
+			SearchRegion wantR3 = new SearchRegion("인천");
+			SearchRegion wantR4 = new SearchRegion("서울");
+			SearchRegion wantR5 = new SearchRegion("충청도");
+			SearchRegion wantR6 = new SearchRegion("대전");
+			SearchRegion wantR7 = new SearchRegion("경상도");
+			SearchRegion wantR8 = new SearchRegion("대구");
+			SearchRegion wantR9 = new SearchRegion("울산");
+			SearchRegion wantR10 = new SearchRegion("부산");
+			SearchRegion wantR11 = new SearchRegion("전라도");
+			SearchRegion wantR12 = new SearchRegion("광주");
 				
 				
-				
-				ArrayList<SearchRegion> sR = new ArrayList<>();
-				sR.add(wantR1);
-				sR.add(wantR2);
-				sR.add(wantR3);
-				sR.add(wantR4);
-				sR.add(wantR5);
-				sR.add(wantR6);
-				sR.add(wantR7);
-				sR.add(wantR8);
-				sR.add(wantR9);
-				sR.add(wantR10);
-				sR.add(wantR11);
-				sR.add(wantR12);
+			ArrayList<SearchRegion> sR = new ArrayList<>();
+			sR.add(wantR1);
+			sR.add(wantR2);
+			sR.add(wantR3);
+			sR.add(wantR4);
+			sR.add(wantR5);
+			sR.add(wantR6);
+			sR.add(wantR7);
+			sR.add(wantR8);
+			sR.add(wantR9);
+			sR.add(wantR10);
+			sR.add(wantR11);
+			sR.add(wantR12);
 				
 //				line.showSearchRegion(sR);
 				
-				for(SearchRegion wantRg: sR) {
-					String name = wantRg.getRegion();
+			for(SearchRegion wantRg: sR) {
+				String name = wantRg.getRegion();
 
-					if(name.equals("강원도") && wantRegion.equals("강원도")) {
-						System.out.println("동해, 평창, 강릉, 속초, 영주, 봉화");
-						GoTrip.printline();
-						break;
-					} else if(name.equals("경기도") && wantRegion.equals("경기도")) {
-						System.out.println("수원, 시흥, 안성, 용인, 파주, 가평, 포천, 과천, 화성, 평택");
-						GoTrip.printline();
-						break;
-					} else if(name.equals("인천") && wantRegion.equals("인천")) {
-						System.out.println("중구, 서구, 계양, 부평, 남동, 동구, 연수");
-						GoTrip.printline();
-						break;
-					} else if(name.equals("서울") && wantRegion.equals("서울")) {
-						System.out.println("강동, 관악, 동대문, 강남, 영등포, 용산, 종로, 마포, 서초, 강북");
-						GoTrip.printline();
-						break;
-					} else if(name.equals("충청도") && wantRegion.equals("충청도")) {
-						System.out.println("단양, 영월, 충주, 청주, 보령, 논산, 서산, 태안, 당진");
-						GoTrip.printline();
-						break;
-					}  else if(name.equals("대전") && wantRegion.equals("대전")) {
-						System.out.println("동구, 중구, 유성구, 대덕구, 서구");
-						GoTrip.printline();
-						break;
-					} else if(name.equals("경상도") && wantRegion.equals("경상도")) {
-						System.out.println("안동, 영주, 경주, 통영, 거제, 하동, 창원, 남해, 사천, 진주");
-						GoTrip.printline();
-						break;
-					} else if(name.equals("대구") && wantRegion.equals("대구")) {
-						System.out.println("동구, 남구, 수성구, 중구, 달성군, 달서구");
-						GoTrip.printline();
-						break;
-					} else if(name.equals("울산") && wantRegion.equals("울산")) {
-						System.out.println("남구, 북구, 울주군, 중구, 동구");
-						GoTrip.printline();
-						break;
-					} else if(name.equals("부산") && wantRegion.equals("부산")) {
-						System.out.println("해운대, 부산진, 강서, 사상, 동래구, 중구, 남구");
-						GoTrip.printline();
-						break;
-					} else if(name.equals("전라도") && wantRegion.equals("전라도")) {
-						System.out.println("전주, 군산, 무주, 목포, 여수, 보성, 해남, 담양, 순천, 고창");
-						GoTrip.printline();
-						break;
-					} else if(name.equals("광주") && wantRegion.equals("광주")) {
-						System.out.println("광산구, 서구, 동구, 북구, 남구");
-						GoTrip.printline();
-						break;
-					}
-					
+				if(name.equals("강원도") && wantRegion.equals("강원도")) {
+					System.out.println("동해, 평창, 강릉, 속초, 영주, 봉화");
+					GoTrip.printline();
+					break;
+				} else if(name.equals("경기도") && wantRegion.equals("경기도")) {
+					System.out.println("수원, 시흥, 안성, 용인, 파주, 가평, 포천, 과천, 화성, 평택");
+					GoTrip.printline();
+					break;
+				} else if(name.equals("인천") && wantRegion.equals("인천")) {
+					System.out.println("중구, 서구, 계양, 부평, 남동, 동구, 연수");
+					GoTrip.printline();
+					break;
+				} else if(name.equals("서울") && wantRegion.equals("서울")) {
+					System.out.println("강동, 관악, 동대문, 강남, 영등포, 용산, 종로, 마포, 서초, 강북");
+					GoTrip.printline();
+					break;
+				} else if(name.equals("충청도") && wantRegion.equals("충청도")) {
+					System.out.println("단양, 영월, 충주, 청주, 보령, 논산, 서산, 태안, 당진");
+					GoTrip.printline();
+					break;
+				}  else if(name.equals("대전") && wantRegion.equals("대전")) {
+					System.out.println("동구, 중구, 유성구, 대덕구, 서구");
+					GoTrip.printline();
+					break;
+				} else if(name.equals("경상도") && wantRegion.equals("경상도")) {
+					System.out.println("안동, 영주, 경주, 통영, 거제, 하동, 창원, 남해, 사천, 진주");
+					GoTrip.printline();
+					break;
+				} else if(name.equals("대구") && wantRegion.equals("대구")) {
+					System.out.println("동구, 남구, 수성구, 중구, 달성군, 달서구");
+					GoTrip.printline();
+					break;
+				} else if(name.equals("울산") && wantRegion.equals("울산")) {
+					System.out.println("남구, 북구, 울주군, 중구, 동구");
+					GoTrip.printline();
+					break;
+				} else if(name.equals("부산") && wantRegion.equals("부산")) {
+					System.out.println("해운대, 부산진, 강서, 사상, 동래구, 중구, 남구");
+					GoTrip.printline();
+					break;
+				} else if(name.equals("전라도") && wantRegion.equals("전라도")) {
+					System.out.println("전주, 군산, 무주, 목포, 여수, 보성, 해남, 담양, 순천, 고창");
+					GoTrip.printline();
+					break;
+				} else if(name.equals("광주") && wantRegion.equals("광주")) {
+					System.out.println("광산구, 서구, 동구, 북구, 남구");
+					GoTrip.printline();
+					break;
 				}
+					
 				
 			}
 			break;
@@ -323,46 +313,38 @@ public class GoTrip {
 		
 		// 검색 내역 실행 (기능 보류)
 		case 5: 
-			if(num == 5) {
-				
-			}
 			break;
 		// 검색 내역 종료
 			
 		// 계절별 나라 추천 기능창 실행
 		case 6:
-			if(num == 6) {
-				Season ss = new Season();
+			Season ss = new Season();
 				
-				GoTrip.printline();
+			GoTrip.printline();
 
-				System.out.println("계절별 추천 여행지");
-				System.out.print("계절 : ");
-				String season = scanf.next();
+			System.out.println("계절별 추천 여행지");
+			System.out.print("계절 : ");
+			String season = scanf.next();
 				
-				GoTrip.printline();
+			GoTrip.printline();
 				
-				System.out.println("<< " + season + " 추천 여행지 >>");
+			System.out.println("<< " + season + " 추천 여행지 >>");
 				
-				if(season.equals("봄")) {
-					ss.spring();
-				} else if(season.equals("여름")) {
-					ss.summer();
-				} else if(season.equals("가을")) {
-					ss.autumn();
-				} else if(season.equals("겨울")) {
-					ss.winter();
-				}
+			if(season.equals("봄")) {
+				ss.spring();
+			} else if(season.equals("여름")) {
+				ss.summer();
+			} else if(season.equals("가을")) {
+				ss.autumn();
+			} else if(season.equals("겨울")) {
+				ss.winter();
 			}
 			break;
-			
+		
 		// 계절 별 추천 지역 기능창 종료
 			
 		// 후기 창 실행 (기능 보류)
 		case 7:
-			if(num == 7) {
-				
-			}
 			break;
 		
 		// 후기 창 종료
