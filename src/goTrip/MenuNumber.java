5f0bfe324d5af005442e409af1f232de0fd835b6
byswiy <byswiy99@naver.com>
2022-04-16 16:23:00 +0900

    MenuNumber.java : 재저장

--- src/goTrip/MenuNumber.java
+++ src/goTrip/MenuNumber.java
@@ -1,5 +1,18 @@
 package goTrip;
 
 public class MenuNumber {
-
+	// 여행 경비 입력 기능 -> 1
+	public static final int travelExpensesInput = 1;
+	// 지역별 인기 추천 장소 기능 -> 2
+	public static final int popularPlaceOfRegion = 2;
+	// 인기 있는 지역 10군데 추천 기능 -> 3
+	public static final int popularPlaceTop10 = 3;
+	// 검색한 지역 별 인기 시, 군 추천 기능 -> 4
+	public static final int searchCityCountryDistrict = 4;
+	// 검색 내역 기능 -> 5
+	public static final int searchHistory = 5;
+	// 계절 선택에 따른 지역 추천 기능 -> 6
+	public static final int searchSeason = 6;
+	// 후기 기능 -> 7
+	public static final int review = 7;
 }
